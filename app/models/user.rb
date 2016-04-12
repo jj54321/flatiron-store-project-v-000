fe7fa9965c0c6dbe27ebb5496d1c86549f5a4843
jj54321 <mailme711@gmail.com>
2016-04-12 09:25:40 -0400

    all model tests passing

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,14 @@ class User < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable
 
+
+  def current_cart=(cart)
+    self.current_cart_id = cart.id if cart
+    @current_cart = cart
+    self.save
+  end
+
+  def current_cart
+    @current_cart = Cart.find_by(id: self.current_cart_id)
+  end
 end
